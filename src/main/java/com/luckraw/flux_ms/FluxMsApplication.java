d6c0ea9285655c0535c8da3996ebac4c97a06b04
Lucas <krawdev@gmail.com>
2024-09-26 12:19:29 -0300

    feat: first commit

---
+++ src/main/java/com/luckraw/flux_ms/FluxMsApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.flux_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckraw/flux_ms/FluxMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.flux_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
