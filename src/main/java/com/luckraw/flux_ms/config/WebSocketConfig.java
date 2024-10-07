58c670ae3c3b986673d52687232f6ac9cde17145
Lucas <krawdev@gmail.com>
2024-10-07 13:44:32 -0300

    feat: small fixes.

--- src/main/java/com/luckraw/flux_ms/config/WebSocketConfig.java
+++ src/main/java/com/luckraw/flux_ms/config/WebSocketConfig.java
@@ -2,16 +2,18 @@ package com.luckraw.flux_ms.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topics");
-        registry.setApplicationDestinationPrefixes("/flux");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,5 @@
 const stompClient = new StompJs.Client({
-    brokerURL: 'ws://' + window.location.host + '/buildrun-livechat-websocket'
+    brokerURL: 'ws://' + window.location.host + '/flux-livechat'
 });
 
 stompClient.onConnect = (frame) => {
